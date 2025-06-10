6d03db576fa5eeb861131d8d330ad164cb00344d
Gustavo <gustavo.st971@gmail.com>
2025-06-09 21:21:43 -0300

    fix: WHERE clause added to findAll function

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -15,6 +15,9 @@ export class TaskService {
     return await this.prisma.tasks.findMany({
       orderBy: {
         id: 'desc',
+      },
+      where: {
+        isArchived: false,
       }
     });
   }
